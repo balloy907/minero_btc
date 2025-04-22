c576b9b7e563feaaa90d7ad8c9e6a2e472548623
balloy907 <mjh33bta@gmail.com>
2025-04-22 14:30:49 -0500

    Añadido xmrig, config.json y start.sh

---
+++ build/CMakeFiles/xmrig.dir/src/backend/opencl/generators/ocl_generic_kawpow_generator.cpp.o.d
@@ -0,0 +1,251 @@
+CMakeFiles/xmrig.dir/src/backend/opencl/generators/ocl_generic_kawpow_generator.cpp.o: \
+ /home/balloy/xmrig/src/backend/opencl/generators/ocl_generic_kawpow_generator.cpp \
+ /usr/include/stdc-predef.h \
+ /home/balloy/xmrig/src/backend/opencl/OclThreads.h \
+ /usr/include/c++/14/vector /usr/include/c++/14/bits/requires_hosted.h \
+ /usr/include/x86_64-linux-gnu/c++/14/bits/c++config.h \
+ /usr/include/x86_64-linux-gnu/c++/14/bits/os_defines.h \
+ /usr/include/features.h /usr/include/features-time64.h \
+ /usr/include/x86_64-linux-gnu/bits/wordsize.h \
+ /usr/include/x86_64-linux-gnu/bits/timesize.h \
+ /usr/include/x86_64-linux-gnu/sys/cdefs.h \
+ /usr/include/x86_64-linux-gnu/bits/long-double.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs.h \
+ /usr/include/x86_64-linux-gnu/gnu/stubs-64.h \
+ /usr/include/x86_64-linux-gnu/c++/14/bits/cpu_defines.h \
+ /usr/include/c++/14/bits/stl_algobase.h \
+ /usr/include/c++/14/bits/functexcept.h \
+ /usr/include/c++/14/bits/exception_defines.h \
+ /usr/include/c++/14/bits/cpp_type_traits.h \
+ /usr/include/c++/14/bits/version.h /usr/include/c++/14/ext/type_traits.h \
+ /usr/include/c++/14/ext/numeric_traits.h \
+ /usr/include/c++/14/bits/stl_pair.h /usr/include/c++/14/type_traits \
+ /usr/include/c++/14/bits/move.h /usr/include/c++/14/bits/utility.h \
+ /usr/include/c++/14/bits/stl_iterator_base_types.h \
+ /usr/include/c++/14/bits/stl_iterator_base_funcs.h \
+ /usr/include/c++/14/bits/concept_check.h \
+ /usr/include/c++/14/debug/assertions.h \
+ /usr/include/c++/14/bits/stl_iterator.h \
+ /usr/include/c++/14/bits/ptr_traits.h /usr/include/c++/14/debug/debug.h \
+ /usr/include/c++/14/bits/predefined_ops.h \
+ /usr/include/c++/14/bits/allocator.h \
+ /usr/include/x86_64-linux-gnu/c++/14/bits/c++allocator.h \
+ /usr/include/c++/14/bits/new_allocator.h /usr/include/c++/14/new \
+ /usr/include/c++/14/bits/exception.h \
+ /usr/include/c++/14/bits/memoryfwd.h \
+ /usr/include/c++/14/bits/stl_construct.h \
+ /usr/include/c++/14/bits/stl_uninitialized.h \
+ /usr/include/c++/14/ext/alloc_traits.h \
+ /usr/include/c++/14/bits/alloc_traits.h \
+ /usr/include/c++/14/bits/stl_vector.h \
+ /usr/include/c++/14/initializer_list \
+ /usr/include/c++/14/bits/stl_bvector.h \
+ /usr/include/c++/14/bits/functional_hash.h \
+ /usr/include/c++/14/bits/hash_bytes.h /usr/include/c++/14/bits/refwrap.h \
+ /usr/include/c++/14/bits/invoke.h \
+ /usr/include/c++/14/bits/stl_function.h \
+ /usr/include/c++/14/backward/binders.h \
+ /usr/include/c++/14/bits/range_access.h \
+ /usr/include/c++/14/bits/vector.tcc \
+ /home/balloy/xmrig/src/backend/opencl/OclThread.h \
+ /home/balloy/xmrig/src/3rdparty/rapidjson/fwd.h \
+ /home/balloy/xmrig/src/3rdparty/rapidjson/rapidjson.h \
+ /usr/include/c++/14/cstdlib /usr/include/stdlib.h \
+ /usr/include/x86_64-linux-gnu/bits/libc-header-start.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/stddef.h \
+ /usr/include/x86_64-linux-gnu/bits/waitflags.h \
+ /usr/include/x86_64-linux-gnu/bits/waitstatus.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn.h \
+ /usr/include/x86_64-linux-gnu/bits/floatn-common.h \
+ /usr/include/x86_64-linux-gnu/bits/types/locale_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__locale_t.h \
+ /usr/include/x86_64-linux-gnu/sys/types.h \
+ /usr/include/x86_64-linux-gnu/bits/types.h \
+ /usr/include/x86_64-linux-gnu/bits/typesizes.h \
+ /usr/include/x86_64-linux-gnu/bits/time64.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clock_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/clockid_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/time_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/timer_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-intn.h /usr/include/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endian.h \
+ /usr/include/x86_64-linux-gnu/bits/endianness.h \
+ /usr/include/x86_64-linux-gnu/bits/byteswap.h \
+ /usr/include/x86_64-linux-gnu/bits/uintn-identity.h \
+ /usr/include/x86_64-linux-gnu/sys/select.h \
+ /usr/include/x86_64-linux-gnu/bits/select.h \
+ /usr/include/x86_64-linux-gnu/bits/types/sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__sigset_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timeval.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_timespec.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes.h \
+ /usr/include/x86_64-linux-gnu/bits/thread-shared-types.h \
+ /usr/include/x86_64-linux-gnu/bits/pthreadtypes-arch.h \
+ /usr/include/x86_64-linux-gnu/bits/atomic_wide_counter.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_mutex.h \
+ /usr/include/x86_64-linux-gnu/bits/struct_rwlock.h /usr/include/alloca.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-bsearch.h \
+ /usr/include/x86_64-linux-gnu/bits/stdlib-float.h \
+ /usr/include/c++/14/bits/std_abs.h /usr/include/c++/14/cstring \
+ /usr/include/string.h /usr/include/strings.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/stdint.h /usr/include/stdint.h \
+ /usr/include/x86_64-linux-gnu/bits/wchar.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-uintn.h \
+ /usr/include/x86_64-linux-gnu/bits/stdint-least.h \
+ /usr/include/inttypes.h /usr/include/c++/14/cassert \
+ /usr/include/assert.h /usr/include/c++/14/utility \
+ /usr/include/c++/14/bits/stl_relops.h /usr/include/c++/14/bitset \
+ /usr/include/c++/14/string /usr/include/c++/14/bits/stringfwd.h \
+ /usr/include/c++/14/bits/char_traits.h \
+ /usr/include/c++/14/bits/postypes.h /usr/include/c++/14/cwchar \
+ /usr/include/wchar.h /usr/lib/gcc/x86_64-linux-gnu/14/include/stdarg.h \
+ /usr/include/x86_64-linux-gnu/bits/types/wint_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__mbstate_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/FILE.h \
+ /usr/include/c++/14/bits/localefwd.h \
+ /usr/include/x86_64-linux-gnu/c++/14/bits/c++locale.h \
+ /usr/include/c++/14/clocale /usr/include/locale.h \
+ /usr/include/x86_64-linux-gnu/bits/locale.h /usr/include/c++/14/iosfwd \
+ /usr/include/c++/14/cctype /usr/include/ctype.h \
+ /usr/include/c++/14/bits/ostream_insert.h \
+ /usr/include/c++/14/bits/cxxabi_forced.h \
+ /usr/include/c++/14/bits/basic_string.h \
+ /usr/include/c++/14/ext/string_conversions.h /usr/include/c++/14/cstdio \
+ /usr/include/stdio.h /usr/include/x86_64-linux-gnu/bits/types/__fpos_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/__fpos64_t.h \
+ /usr/include/x86_64-linux-gnu/bits/types/struct_FILE.h \
+ /usr/include/x86_64-linux-gnu/bits/types/cookie_io_functions_t.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio_lim.h \
+ /usr/include/x86_64-linux-gnu/bits/stdio.h /usr/include/c++/14/cerrno \
+ /usr/include/errno.h /usr/include/x86_64-linux-gnu/bits/errno.h \
+ /usr/include/linux/errno.h /usr/lib/linux/uapi/x86/asm/errno.h \
+ /usr/include/asm-generic/errno.h /usr/include/asm-generic/errno-base.h \
+ /usr/include/x86_64-linux-gnu/bits/types/error_t.h \
+ /usr/include/c++/14/bits/charconv.h \
+ /usr/include/c++/14/bits/basic_string.tcc \
+ /home/balloy/xmrig/src/backend/opencl/wrappers/OclDevice.h \
+ /home/balloy/xmrig/src/backend/common/misc/PciTopology.h \
+ /home/balloy/xmrig/src/base/tools/String.h \
+ /home/balloy/xmrig/src/backend/opencl/wrappers/OclVendor.h \
+ /usr/include/c++/14/algorithm /usr/include/c++/14/bits/stl_algo.h \
+ /usr/include/c++/14/bits/algorithmfwd.h \
+ /usr/include/c++/14/bits/stl_heap.h \
+ /usr/include/c++/14/bits/uniform_int_dist.h \
+ /usr/include/c++/14/bits/stl_tempbuf.h \
+ /home/balloy/xmrig/src/base/crypto/Algorithm.h \
+ /usr/include/c++/14/functional /usr/include/c++/14/tuple \
+ /usr/include/c++/14/bits/uses_allocator.h \
+ /usr/include/c++/14/bits/std_function.h /usr/include/c++/14/typeinfo \
+ /home/balloy/xmrig/src/crypto/randomx/randomx.h \
+ /usr/include/c++/14/cstddef /usr/include/c++/14/cstdint \
+ /home/balloy/xmrig/src/crypto/randomx/intrin_portable.h \
+ /home/balloy/xmrig/src/crypto/randomx/blake2/endian.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/x86intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/x86gprintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/ia32intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/adxintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/bmiintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/bmi2intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/cetintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/cldemoteintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/clflushoptintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/clwbintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/clzerointrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/cmpccxaddintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/enqcmdintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/fxsrintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/lzcntintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/lwpintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/movdirintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/mwaitintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/mwaitxintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/pconfigintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/popcntintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/pkuintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/prfchiintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/raointintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/rdseedintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/rtmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/serializeintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/sgxintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/tbmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/tsxldtrkintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/uintrintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/waitpkgintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/wbnoinvdintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/xsaveintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/xsavecintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/xsaveoptintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/xsavesintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/xtestintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/hresetintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/usermsrintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/immintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/mmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/xmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/mm_malloc.h \
+ /usr/include/c++/14/stdlib.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/emmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/pmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/tmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/smmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/wmmintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avxintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avxvnniintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avxifmaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avxvnniint8intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avxvnniint16intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx2intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512fintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512erintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512pfintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512cdintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512bwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512dqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vlbwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vldqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512ifmaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512ifmavlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vbmiintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vbmivlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx5124fmapsintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx5124vnniwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vpopcntdqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vbmi2intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vbmi2vlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vnniintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vnnivlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vpopcntdqvlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512bitalgintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512bitalgvlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vp2intersectintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512vp2intersectvlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512fp16intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512fp16vlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/shaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/sm3intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/sha512intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/sm4intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/fmaintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/f16cintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/gfniintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/vaesintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/vpclmulqdqintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512bf16vlintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avx512bf16intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/avxneconvertintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/amxtileintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/amxint8intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/amxbf16intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/amxcomplexintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/prfchwintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/keylockerintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/amxfp16intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/mm3dnow.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/fma4intrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/ammintrin.h \
+ /usr/lib/gcc/x86_64-linux-gnu/14/include/xopintrin.h \
+ /home/balloy/xmrig/src/crypto/rx/RxAlgo.h
